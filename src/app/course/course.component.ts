35d6759f21a9050f8d97df146003da7987a2175d
Jessica Ryan <jessica.ryan@dazn.com>
2019-02-22 12:20:40 +0000

    Finish up section 3 and 4

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,10 +13,10 @@ import {
     withLatestFrom,
     concatAll, shareReplay, throttle, throttleTime
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, interval} from 'rxjs';
+import {merge, fromEvent, Observable, concat, interval, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
-
+import { debug, RxJsLoggingLevel, setRxJsLoggingLevel } from '../common/debug';
 
 @Component({
     selector: 'course',
@@ -36,8 +36,24 @@ export class CourseComponent implements OnInit, AfterViewInit {
   ngOnInit() {
 
     this.courseId = this.route.snapshot.params['id'];
-    this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    // this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+      // .pipe(
+      //   debug(RxJsLoggingLevel.INFO, 'course value')
+      // );
 
+    // setRxJsLoggingLevel(RxJsLoggingLevel.DEBUG);
+
+    const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+    const lessons$ = this.loadLessons();
+
+    forkJoin(course$, lessons$)
+      .pipe(
+        tap(([course, lessons]) => {
+          console.log(course);
+          console.log(lessons);
+        })
+      )
+      .subscribe();
   }
 
   ngAfterViewInit() {
@@ -45,14 +61,16 @@ export class CourseComponent implements OnInit, AfterViewInit {
     //   .pipe(
     //     map(event => event.target.value),
     //     startWith(''),
+    //     debug(RxJsLoggingLevel.TRACE, 'search'),
     //     debounceTime(400),
     //     distinctUntilChanged(),
-    //     switchMap(search => this.loadLessons(search))
+    //     switchMap(search => this.loadLessons(search)),
+    //     debug(RxJsLoggingLevel.DEBUG, 'lessons value')
     //   );
     fromEvent<any>(this.input.nativeElement, 'keyup')
       .pipe(
         map(event => event.target.value),
-        startWith(''),
+        // startWith(''),
         throttleTime(500)
       )
       .subscribe(console.log);
